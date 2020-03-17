96f76db6ac5f42226df9df22510a6591824b1d11
pudjapu <62283696+pudjapu@users.noreply.github.com>
2020-03-17 17:29:37 +0700

    new data

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+require './connect.php';
+
+session_start();
+
+$sql = "SELECT * FROM account WHERE ID = '$IDuse'";
+
+$result = mysqli_query($connect,$sql);
+
+if($result){
+    while($record = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+        echo "ID : ".$record['ID']."<br>";
+    }
+} else {
+    echo "-----";
+}
+
+?>
+
+//55555555555555555555555555555555
